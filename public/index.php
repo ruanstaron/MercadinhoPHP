a1669600c74adfb4cedd069c646669248ed0f139
Ruan Staron <ruan.staron@hotmail.com>
2017-04-20 00:39:44 -0300

    - Alterado De Soap para restfull - Implementado Slim Framework - Método Get / getProdutos funcionando

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/getprodutos', 'getProdutos');
+$app->run();
+
+function getConn() {
+	return new PDO('mysql:host=localhost;dbname=mercadinho',
+		'root',
+		'',
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}
+
+function getProdutos() {
+	$stmt = getConn()->query("SELECT * FROM produtos");
+	$produtos = $stmt->fetchAll(PDO::FETCH_OBJ);
+	echo "{Produtos:".json_encode($produtos)."}";
+}
+
+?>
