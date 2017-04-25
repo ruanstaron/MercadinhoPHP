7ede67f2b11b2b622a8c4a18ce2db97b6a2bd71e
Ruan Staron <ruan.staron@hotmail.com>
2017-04-24 22:51:48 -0300

    mètodo post addproduto, cagado

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/getprodutos', 'getProdutos');
+$app->post('/addproduto','addProduto');
 $app->run();
 
 function getConn() {
@@ -26,6 +27,19 @@ function getProdutos() {
 	$stmt = getConn()->query("SELECT * FROM produtos");
 	$produtos = $stmt->fetchAll(PDO::FETCH_OBJ);
 	echo json_encode($produtos);
+}
+
+function addProduto(){
+	$request = \Slim\Slim::getInstance()->request();
+	$produto = json_decode($request->getBody());
+	$sql = "INSERT INTO produtos (cod_barras, produto) values (:cod_barras,':produto')";
+	$conn = getConn();
+	$stmt = $conn->prepare($sql);
+	$stmt->bindParam("cod_barras",$produto->cod_barras);
+	$stmt->bindParam("produto",$produto->produto);
+	$stmt->execute();
+	$produto->id = $conn->lastInsertId();
+	echo json_encode($produto);
 }
 
 ?>
