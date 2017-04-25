e03ed08f7d612a306dd60af30a6cf69da6188360
Ruan Staron <ruan.staron@hotmail.com>
2017-04-24 22:15:01 -0300

    - Desvercionei a pasta vendors - Corrigido problema com a senha do banco, agora estamos tratando a senha através de uma exception - Agora está retornando um json com todos os produtos.

--- public/index.php
+++ public/index.php
@@ -9,16 +9,23 @@ $app->get('/getprodutos', 'getProdutos');
 $app->run();
 
 function getConn() {
+try{
 	return new PDO('mysql:host=localhost;dbname=mercadinho',
 		'root',
 		'',
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}catch(Exception $e){
+	return new PDO('mysql:host=localhost;dbname=mercadinho',
+		'root',
+		'chalaheadchala2',
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}
 }
 
 function getProdutos() {
 	$stmt = getConn()->query("SELECT * FROM produtos");
 	$produtos = $stmt->fetchAll(PDO::FETCH_OBJ);
-	echo "{Produtos:".json_encode($produtos)."}";
+	echo json_encode($produtos);
 }
 
 ?>
